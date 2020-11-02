77c5a61989d31fb51a9d506bc1f6e76ed1b9e37b
Khushi <dpskhu13108@gmail.com>
2020-11-02 23:54:50 +0530

    hard push

---
+++ imageupload/backend/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const multer = require("multer");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+const upload = multer() 
+router.post("/upload",upload.single("file"), function(req, res, next) {
+  console.log("req.body");
+});
+
+module.exports = router;
